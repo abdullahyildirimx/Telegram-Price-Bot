9499a05f78d6044f1181608801922926acb7e82b
Abdullah <ayild1008@gmail.com>
2023-11-13 03:25:58 +0300

    binance code clearing, lists updated

--- binance.py
+++ binance.py
@@ -3,30 +3,42 @@ import urllib.request
 from lists import binancelist
 
 def handleBinance(message, resultMessage):
-    if (message.text.upper() in binancelist) or (message.text.upper()=="USDT") or (message.text.upper() == "ETHBTC"):
-        upperMessage = message.text.upper()
+    upperMessage = message.text.upper()
+    speciallist = ["USDT", "ETHBTC", "TORN", "PIVX"]
+    if (upperMessage in binancelist) or (upperMessage in speciallist):
         user_agent = 'Mozilla/5.0'
         url = "https://api.binance.com/api/v3/ticker/24hr?symbol=" + upperMessage + "USDT"
         if upperMessage == "USDT":
             url = "https://api.binance.com/api/v3/ticker/24hr?symbol=" + "USDT" + "TRY"
-        headers={'User-Agent':user_agent,}
+        if upperMessage == "PIVX":
+            url = "https://api.binance.com/api/v3/ticker/24hr?symbol=" + "PIVX" + "BTC"
         if upperMessage == "ETHBTC":
             url = "https://api.binance.com/api/v3/ticker/24hr?symbol=" + "ETH" + "BTC"
+        if upperMessage == "TORN":
+            url = "https://api.binance.com/api/v3/ticker/24hr?symbol=" + "TORN" + "BUSD"
         headers={'User-Agent':user_agent,}
         request=urllib.request.Request(url,None,headers)
         response = urllib.request.urlopen(request)
         data = response.read()
         output = json.loads(data)
-        
         symbol = ""
+        price = ""
         if upperMessage == "USDT":
             symbol = "₺"
+            price = format(float(output['lastPrice']))
+        elif upperMessage == "PIVX":
+            symbol = "₿"
+            price = format(int(10**8*float(output['lastPrice'])))
+        elif upperMessage == "ETHBTC":
+            symbol = "₿"
+            price = format(float(output['lastPrice']))
         else:
             symbol = "$"
-
+            price = format(float(output['lastPrice']))
+        percent = "  %{:.2f}".format(float(output['priceChangePercent']))
         if resultMessage != "":
             resultMessage += "\n"
-        resultMessage += "Binance -> " + upperMessage + ': ' + symbol + format(float(output['lastPrice'])) + "  %{:.2f}".format(float(output['priceChangePercent']))
+        resultMessage += "Binance -> " + upperMessage + ': ' + symbol + price + percent
 
     if message.text.upper()=="GAINERS" or message.text.upper()=="LOSERS":
         changePercentList = []
@@ -46,14 +58,14 @@ def handleBinance(message, resultMessage):
             resultMessage += "\n"
         if message.text.upper()=="GAINERS":
             changePercentList.sort(key= lambda coin: coin[2], reverse=True)
-            resultMessage += "Binance Top Gainers:\n"
+            resultMessage += "Binance Top Gainers:"
         if message.text.upper()=="LOSERS":
             changePercentList.sort(key= lambda coin: coin[2])
-            resultMessage += "Binance Top Losers:\n"
+            resultMessage += "Binance Top Losers:"
 
-        resultMessage += changePercentList[0][0] + ': ' + format(changePercentList[0][1]) + "  %{:.2f}".format(float(changePercentList[0][2])) + "\n"
-        resultMessage += changePercentList[1][0] + ': ' + format(changePercentList[1][1]) + "  %{:.2f}".format(float(changePercentList[1][2])) + "\n"
-        resultMessage += changePercentList[2][0] + ': ' + format(changePercentList[2][1]) + "  %{:.2f}".format(float(changePercentList[2][2])) + "\n"
-        resultMessage += changePercentList[3][0] + ': ' + format(changePercentList[3][1]) + "  %{:.2f}".format(float(changePercentList[3][2])) + "\n"
-        resultMessage += changePercentList[4][0] + ': ' + format(changePercentList[4][1]) + "  %{:.2f}".format(float(changePercentList[4][2]))
+        for i in range(5):
+            if resultMessage != "":
+                resultMessage += "\n"
+            resultMessage += changePercentList[i][0] + ': ' + format(changePercentList[i][1]) + "  %{:.2f}".format(float(changePercentList[i][2])) + "\n"
+
     return resultMessage
