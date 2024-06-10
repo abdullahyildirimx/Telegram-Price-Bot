d695821f3e9658ee7effa7af8c6e8754b2a87beb
Abdullah Yıldırım <ayild1008@gmail.com>
2024-06-10 22:59:06 +0300

    Etherscan gas method change

--- gas.py
+++ gas.py
@@ -1,22 +1,14 @@
-import json
-import urllib.request
+import requests
 from lists import gaslist
 from keys import etherscankey
 
 def handleGas(message, resultMessage):
-    if message.text.upper()=="GAS":
-        upperMessage = message.text.upper()
-        user_agent = 'Mozilla/5.0'
+    if message.text.upper() in gaslist:
         url = "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=" + etherscankey
-        headers={'User-Agent':user_agent,} 
-        request=urllib.request.Request(url,None,headers)
-        response = urllib.request.urlopen(request)
-        data = response.read()
-        output = json.loads(data)
-
+        response = requests.get(url)
+        output = response.json()
         if resultMessage != "":
             resultMessage += "\n"
-
         resultMessage += "Low: " + output['result']['SafeGasPrice'] + " Gwei\n"
         resultMessage += "Average: " + output['result']['ProposeGasPrice'] + " Gwei\n"
         resultMessage += "High: " + output['result']['FastGasPrice'] + " Gwei\n"
